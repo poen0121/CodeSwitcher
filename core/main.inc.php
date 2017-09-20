ed8d5eed2a4dcb4b709dbb8b6f94cbe014655807
Poen <poen0121@gmail.com>
2017-09-20 17:04:53 +0800

    Update file

--- core/main.inc.php
+++ core/main.inc.php
@@ -43,7 +43,7 @@ if (!class_exists('csl_mvc')) {
 		private static function start() {
 			if (is_null(self :: $portal)) {
 				clearstatcache();
-				csl_error :: cast_log_title('PHP-CS');
+				csl_error :: cast_log_title('CS-PHP');
 				csl_error :: trace(false); //system default error stack trace mode
 				csl_error :: error_log_file(BASEPATH . 'storage/logs/CS-' . csl_time :: get_date('host') . '.log', true); //peel of system log file mode
 				csl_debug :: report(true); //error mode E_ALL
